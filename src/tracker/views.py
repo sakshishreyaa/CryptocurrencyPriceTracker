8073ab2fc1d50dde92b737273a8a0c204626d789
sakshishreyaa <sakshishreyaaaaa@gmail.com>
2022-10-15 18:47:44 +0530

    implement pagination

--- src/tracker/views.py
+++ src/tracker/views.py
@@ -7,10 +7,10 @@ from .models import CryptoTracker
 def get(request, *args, **kwargs):
     data=request.GET
     date=data.get('date')
-    limit=data.get('limit',100)
-    offset=data.get('offset',0)
+    limit=int(data.get('limit',100))
+    offset=int(data.get('offset',0))
     date =datetime.strptime(date,"%d-%m-%Y")
-    resp = CryptoTracker.objects.filter().all()
+    resp = CryptoTracker.objects.filter().all()[offset:offset+limit]
 
     data=[]
     for i in resp:
