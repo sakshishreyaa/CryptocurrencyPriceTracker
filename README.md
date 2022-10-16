e5529bc27d056d9d2a1a0a086c11c22a5a75304f
sakshishreyaa <sakshishreyaaaaa@gmail.com>
2022-10-16 10:14:07 +0530

    pagination

---
+++ README.md
@@ -0,0 +1,3 @@
+to run this project 
+docker-compose up
+run migrations inside db container
--- src/tracker/views.py
+++ src/tracker/views.py
@@ -2,17 +2,35 @@ from datetime import datetime
 from django.http import JsonResponse
 
 from .models import CryptoTracker
-
+from .config import DOMAIN
 
 def get(request, *args, **kwargs):
     data=request.GET
     date=data.get('date')
     limit=int(data.get('limit',100))
     offset=int(data.get('offset',0))
+    current_url = f"{DOMAIN}/api/prices/btc?date={date}&offset={offset}&limit={limit}"
+    next_url = f"{DOMAIN}/api/prices/btc?date={date}&offset={offset+limit}&limit={limit}"
+
     date =datetime.strptime(date,"%d-%m-%Y")
-    resp = CryptoTracker.objects.filter().all()[offset:offset+limit]
+    resp = CryptoTracker.objects.all()[offset:offset+limit]
+    count = CryptoTracker.objects.count()
+    print(len(resp),count)
 
     data=[]
     for i in resp:
         data.append({'name':i.name,'price':i.price,'date':i.created_at})
-    return JsonResponse(data,safe=False)
+    final_resp = {
+	"url": current_url,     
+    # current url
+    
+    "next": next_url,  
+    # next url in pagination
+      
+    "count": count,                                                                        
+    # total no of records for the given query
+                    	
+    "data": data,                                                                           
+    # array of price objects
+ }
+    return JsonResponse(final_resp,safe=False)
